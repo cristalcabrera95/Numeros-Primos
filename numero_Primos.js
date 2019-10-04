1baec163f90925d9a6cfc058d7d762be43a93ef5
cristalcabrera95 <cristalcabrera9507@gmail.com>
2019-10-04 13:00:16 -0500

    funcion numeros primos

---
+++ numero_Primos.js
@@ -0,0 +1,13 @@
+let numeros_Primos = [];
+for(let i = 2; i <=100; i++){ 
+  if (i%2==0){
+    numeros_Primos.push(i)
+  }
+  console.log(numeros_Primos)
+};
+
+for(let j = 2; j <=100; j++){ 
+  if (j%2==0){
+    document.write(j + " ");
+  };
+}
